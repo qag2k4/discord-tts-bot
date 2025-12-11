4af7ae29dd0bcbbf9dccf97190c9607148e72f8e
qag2k4 <qag2k4@gmail.com>
2025-12-12 05:47:28 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,10 +2,9 @@
 # exit on error
 set -o errexit
 
-# 1. Cài đặt các thư viện Python từ requirements.txt
 pip install -r requirements.txt
 
-# 2. Tải và cài đặt FFmpeg (Phiên bản Linux)
+# Tải FFmpeg về thư mục ./bin
 echo "Downloading FFmpeg..."
 mkdir -p bin
 curl -L https://github.com/BtbN/FFmpeg-Builds/releases/download/latest/ffmpeg-master-latest-linux64-gpl.tar.xz -o ffmpeg.tar.xz
@@ -15,6 +14,5 @@ mv ffmpeg-master-latest-linux64-gpl/bin/ffprobe ./bin/ffprobe
 chmod +x ./bin/ffmpeg
 chmod +x ./bin/ffprobe
 
-# Dọn dẹp file rác
 rm -rf ffmpeg-master-latest-linux64-gpl ffmpeg.tar.xz
-echo "FFmpeg installed successfully!"
+echo "FFmpeg installed!"
